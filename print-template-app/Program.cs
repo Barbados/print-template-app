0ec19c2a2d880db6cb16b9a5aa3f81593aae9c37
Barbados <vlacomov@gmail.com>
2025-03-30 22:49:59 +0300

    update .gitignore, templates folder settings

--- print-template-app/Program.cs
+++ print-template-app/Program.cs
@@ -1,26 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Dynamic;
-using System.Text.Json;
+﻿using System.Text.Json;
 using FastReport;
 using FastReport.Export.PdfSimple;
 using print_template_app.Model;
 
 var templatePath = string.Empty; // todo: provide path to the template
-var report = new Report();
+using var report = new Report();
 // load report from template path
 report.Load(templatePath);
 
 // prepare data for the report
 var reportData = new ReportData();
 var reportDataObject = JsonSerializer.Serialize(reportData.Params);
+
 // load report data as json data source
 report.Dictionary.Connections[0].ConnectionString = reportDataObject;
 
 report.Prepare();
 
-var pdfExport = new PDFSimpleExport();
-
+// export a document to .pdf
+const string documentName = "test.pdf";
+using var pdfExport = new PDFSimpleExport();
+pdfExport.Export(report, documentName);
 
 Console.WriteLine("Hello, World!");
